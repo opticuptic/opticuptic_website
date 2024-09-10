d8faddd29b2b95e74669e7c938ee843587b09741
opticuptic <sarah@opticuptic.com>
2024-09-10 13:06:45 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'data.json'
-  })
+  container: document.getElementById("bm"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "data.json",
+});
